2c927b1f11919fb74f4664cd549d0fb47e92cf05
Danny <thatoftohave@gmail.com>
2023-07-07 21:24:34 -0700

    Added a simple GET request endpoint to the server to test its correct operation.

--- server/index.js
+++ server/index.js
@@ -7,3 +7,7 @@ const app = express();
 app.listen(PORT, () => {
   console.log(`Server running on ${PORT}`);
 });
+
+app.get("/api", (req, res) => {
+  res.json({ message: "Test server hello." });
+});
