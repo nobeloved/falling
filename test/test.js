aae63099bff26723c094c546e184d98374f6182f
Danny <thatoftohave@gmail.com>
2023-07-08 20:53:10 -0700

    Created a new file to test out a few simple functions and wrote unit tests for them in another new file.

---
+++ test/test.js
@@ -0,0 +1,38 @@
+const basic = require('../math/basic');
+
+let nums1 = '2,2';
+let nums2 = '2,-2';
+let nums3 = '-1,-1';
+let nums4 = '0,7';
+
+test('Adding two numbers (' + nums1 + ') gives their sum.', () => {
+    expect(basic.add(nums1)).toBe(4);
+})
+
+test('Subtracting two numbers (' + nums1 + ') gives their difference.', () => {
+    expect(basic.subtract(nums1)).toBe(0);
+})
+
+test('Adding two numbers (' + nums2 + ') gives their sum.', () => {
+    expect(basic.add(nums2)).toBe(0);
+})
+
+test('Subtracting two numbers (' + nums2 + ') gives their difference.', () => {
+    expect(basic.subtract(nums2)).toBe(4);
+})
+
+test('Adding two numbers (' + nums3 + ') gives their sum.', () => {
+    expect(basic.add(nums3)).toBe(-2);
+})
+
+test('Subtracting two numbers (' + nums3 + ') gives their difference.', () => {
+    expect(basic.subtract(nums3)).toBe(0);
+})
+
+test('Adding two numbers (' + nums4 + ') gives their sum.', () => {
+    expect(basic.add(nums4)).toBe(7);
+})
+
+test('Subtracting two numbers (' + nums4 + ') gives their difference.', () => {
+    expect(basic.subtract(nums4)).toBe(-7);
+})
