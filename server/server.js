65767166ab20e032e187dc1bcb725a24122538d1
Danny <thatoftohave@gmail.com>
2023-07-10 18:28:38 -0700

    Created a new blank default React frontend with basic default files.

--- server/server.js
+++ server/server.js
@@ -5,7 +5,7 @@ const PORT = process.env.PORT || 3000;
 const app = express();
 
 app.listen(PORT, () => {
-  console.log(`Server running on ${PORT}`);
+  console.log(`Server running on ${PORT}.`);
 });
 
 app.get("/api", (req, res) => {
